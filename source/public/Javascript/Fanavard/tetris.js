6a40f7afa18460d29c732015fca7fdeeea1b7124
joseph <bomber.man87@yahoo.com>
2018-07-02 21:32:27 +0430

    material coloring , Ui improvements

--- source/public/Javascript/Fanavard/tetris.js
+++ source/public/Javascript/Fanavard/tetris.js
@@ -68,7 +68,7 @@ var TetrisGame;
 
 
             // choose random color
-            var color = "#ccc";
+            var color = TetrisGame.materialColor();
 
 
             self.column = initColumn;
@@ -119,12 +119,8 @@ var TetrisGame;
          * Choose random words in game build to work with
          */
         chooseWords: function () {
-            var result;
-            var count = 0;
-            for (var prop in TetrisGame.words)
-                if (Math.random() < 1/++count)
-                    result = prop;
-            return TetrisGame.words[result];
+            var keys = Object.keys(TetrisGame.words)
+            return TetrisGame.words[keys[ keys.length * Math.random() << 0]];
         },
 
 
@@ -137,13 +133,100 @@ var TetrisGame;
 
 
         /**
+         * Get a random material color
+         * @return {string}
+         */
+        materialColor: function () {
+            // colors from https://github.com/egoist/color-lib/blob/master/color.json
+            var colors = [
+                "#ef5350",
+                "#d32f2f",
+                "#b71c1c",
+                "#d50000",
+                "#ec407a",
+                "#e91e63",
+                "#d81b60",
+                "#c2185b",
+                "#ad1457",
+                "#880e4f",
+                "#e91e63",
+                "#f50057",
+                "#c51162",
+                "#ab47bc",
+                "#9c27b0",
+                "#8e24aa",
+                "#7b1fa2",
+                "#6a1b9a",
+                "#4a148c",
+                "#9c27b0",
+                "#aa00ff",
+                "#7e57c2",
+                "#673ab7",
+                "#5e35b1",
+                "#512da8",
+                "#4527a0",
+                "#311b92",
+                "#673ab7",
+                "#7c4dff",
+                "#651fff",
+                "#6200ea",
+                "#5c6bc0",
+                "#3f51b5",
+                "#3949ab",
+                "#303f9f",
+                "#283593",
+                "#1a237e",
+                "#3f51b5",
+                "#536dfe",
+                "#3d5afe",
+                "#304ffe",
+                "#2196f3",
+                "#1e88e5",
+                "#1976d2",
+                "#1565c0",
+                "#0d47a1",
+                "#448aff",
+                "#2979ff",
+                "#2962ff",
+                "#039be5",
+                "#0288d1",
+                "#0277bd",
+                "#01579b",
+                "#0091ea",
+                "#00acc1",
+                "#0097a7",
+                "#00838f",
+                "#006064",
+                "#009688",
+                "#00897b",
+                "#00796b",
+                "#00695c",
+                "#004d40",
+                "#4caf50",
+                "#43a047",
+                "#388e3c",
+                "#2e7d32",
+                "#7cb342",
+                "#689f38",
+                "#558b2f",
+                "#33691e",
+                "#9e9d24",
+                "#ef6c00",
+                "#e65100"
+            ];
+            var random = parseInt(Math.random() * colors.length);
+            return colors[random];
+        },
+
+
+        /**
          * Start Game play
          */
         startGamePlay: function () {
 
-            // get max json word length [min:8 - max:12] to create columns
-            var validColumns = TetrisGame.getValidColumnsNumber();
-
+            // get max json word length [min:8 - max:15] to create columns
+            var validColumnsCount = TetrisGame.getValidColumnsNumber();
+            log(validColumnsCount);
 
             // get two words from json to create rows and columns
             log("random words");
@@ -151,8 +234,8 @@ var TetrisGame;
 
 
             // create first char block
-            new TetrisGame.charBlock();
-
+            var char = new TetrisGame.charBlock();
+            log(char);
 
             document.addEventListener("keydown" , function (e) {
                 TetrisGame.aliveChars[TetrisGame.activeCharIndex - 1].move(e.keyCode);
@@ -191,6 +274,15 @@ var TetrisGame;
                        </div>
                    </div>
                    <div class="playBoard"> 
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
+                       <div class="charBlock" >ت</div>
                        <div class="charBlock" >ت</div>
                     </div>
                 </div>
