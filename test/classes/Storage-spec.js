a5939aa81fdab388a0b766fe5c88999e14a28a92
JafarAkhondali <jafar.akhondali@yahoo.com>
2018-07-12 01:14:55 +0430

    + Add development mode to Karma webpack config + Add new test for Storage

--- test/classes/Storage-spec.js
+++ test/classes/Storage-spec.js
@@ -1,7 +1,17 @@
 import Storage from "../../src/public/javascript/classes/Storage";
 
 describe("Storage Class ", function() {
-    it("method set: Saves an item in localStorage", function() {
+    it("method get: with wrong key, it shold return value of second argument", function() {
         expect(Storage.get("notFoo","defaultFoo")).toBe("defaultFoo");
+    });
+});
+
+
+
+describe("Storage Class", function() {
+    Storage.set("my_key","my_value");
+
+    it("method set: with default value saves an item in localStorage", function() {
+        expect(Storage.get("my_key")).toBe("my_value");
     });
 });
