96493414974d19d37c29da14098cdb5487ab3fbc
Mohammad Toosi <m_dd@rogers.com>
2018-07-13 00:14:21 +0430

    storage methods unit test added in one describe

--- test/classes/Storage-spec.js
+++ test/classes/Storage-spec.js
@@ -1,19 +1,13 @@
 import Storage from "../../src/javascript/classes/Storage";
 
-describe("Storage Class", function() {
-    Storage.set("my_key","my_value");
+describe("Storage Class", function () {
+    Storage.set("my_key", "my_value");
 
-    it("method set: with default value saves an item in localStorage", function() {
+    it("method set: with default value saves an item in localStorage", function () {
         expect(Storage.get("my_key")).toBe("my_value");
     });
-});
 
-
-
-describe("Storage Class ", function() {
-    it("method get: with wrong key, it shold return value of second argument", function() {
-        expect(Storage.get("notFoo","defaultFoo")).toBe("defaultFoo");
+    it("method get: with wrong key, it shold return value of second argument", function () {
+        expect(Storage.get("notFoo", "defaultFoo")).toBe("defaultFoo");
     });
 });
-
-
