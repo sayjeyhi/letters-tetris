948c6d5c2d36634a34459a3411b70ed1197c9a62
JafarAkhondali <jafar.akhondali@yahoo.com>
2018-07-18 17:01:03 +0430

    + Copy from /wiki/_book instead of /src/assets/wiki

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CompressionPlugin = require("compression-webpack-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
 
 // Is the current build a development build
 const IS_DEV = (process.env.NODE_ENV === 'dev');
@@ -17,104 +17,104 @@ const appHtmlTitle = 'Webpack Boilerplate';
  * Webpack Configuration
  */
 module.exports = {
-    entry: {
-        bundle: path.join(dirApp, 'javascript/loading/ArshLoader.js')
-    },
-    resolve: {
-        modules: [
-            dirNode,
-            dirApp,
-            dirAssets
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            IS_DEV: IS_DEV
-        }),
+	entry: {
+		bundle: path.join(dirApp, 'javascript/loading/ArshLoader.js')
+	},
+	resolve: {
+		modules: [
+			dirNode,
+			dirApp,
+			dirAssets
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			IS_DEV
+		}),
 
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.ejs'),
-            title: appHtmlTitle
-        }),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'index.ejs'),
+			title: appHtmlTitle
+		}),
 
-        new CopyWebpackPlugin([
-            {from: './src/assets/', to: './assets/' },
-            {from: './src/assets/wiki', to: './wiki/' },
-            {from: './src/serviceWorker.js', to: './serviceWorker.js' },
-            {from: './src/manifests.json', to: './manifests.json' },
-            {from: './src/robots.txt', to: './robots.txt' },
-            {from: './src/humans.txt', to: './humans.txt' }
-        ]),
+		new CopyWebpackPlugin([
+			{ from: './src/assets/', to: './assets/' },
+			{ from: './wiki/_book', to: './wiki/' },
+			{ from: './src/serviceWorker.js', to: './serviceWorker.js' },
+			{ from: './src/manifests.json', to: './manifests.json' },
+			{ from: './src/robots.txt', to: './robots.txt' },
+			{ from: './src/humans.txt', to: './humans.txt' }
+		]),
 
-        new CompressionPlugin({
-            asset: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: /\.js$|\.css$|\.html$|\.json$/
-        })
-    ],
-    module: {
-        rules: [
-            // {
-            //     test: /\.js$/,
-            //     include: path.join(__dirname, 'src/javascript'),
-            //     exclude: /node_modules/,
-            //     use: ['babel-loader', 'eslint-loader']
-            // }
+		new CompressionPlugin({
+			asset: '[path].gz[query]',
+			algorithm: 'gzip',
+			test: /\.js$|\.css$|\.html$|\.json$/
+		})
+	],
+	module: {
+		rules: [
+			// {
+			//     test: /\.js$/,
+			//     include: path.join(__dirname, 'src/javascript'),
+			//     exclude: /node_modules/,
+			//     use: ['babel-loader', 'eslint-loader']
+			// }
 
-            // // BABEL
-            // {
-            //     test: /\.js$/,
-            //     loader: 'babel-loader',
-            //     include: path.join(__dirname, 'test'),
-            //     exclude: /(node_modules)/,
-            //     options: {
-            //         compact: true,
-            //         presets: ['preset-env']
-            //     }
-            // },
-            // // STYLES
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         'style-loader',
-            //         {
-            //             loader: 'css-loader',
-            //             options: {
-            //                 sourceMap: IS_DEV
-            //             }
-            //         },
-            //     ]
-            // },
-            //
-            // // CSS / SASS
-            // {
-            //     test: /\.scss/,
-            //     use: [
-            //         'style-loader',
-            //         {
-            //             loader: 'css-loader',
-            //             options: {
-            //                 sourceMap: IS_DEV
-            //             }
-            //         },
-            //         {
-            //             loader: 'sass-loader',
-            //             options: {
-            //                 sourceMap: IS_DEV,
-            //                 includePaths: [dirAssets]
-            //             }
-            //         }
-            //     ]
-            // },
-            //
-            // // IMAGES
-            // {
-            //     test: /\.(jpe?g|png|gif)$/,
-            //     loader: 'file-loader',
-            //     options: {
-            //         name: '[path][name].[ext]'
-            //     }
-            // }
-        ]
-    }
+			// // BABEL
+			// {
+			//     test: /\.js$/,
+			//     loader: 'babel-loader',
+			//     include: path.join(__dirname, 'test'),
+			//     exclude: /(node_modules)/,
+			//     options: {
+			//         compact: true,
+			//         presets: ['preset-env']
+			//     }
+			// },
+			// // STYLES
+			// {
+			//     test: /\.css$/,
+			//     use: [
+			//         'style-loader',
+			//         {
+			//             loader: 'css-loader',
+			//             options: {
+			//                 sourceMap: IS_DEV
+			//             }
+			//         },
+			//     ]
+			// },
+			//
+			// // CSS / SASS
+			// {
+			//     test: /\.scss/,
+			//     use: [
+			//         'style-loader',
+			//         {
+			//             loader: 'css-loader',
+			//             options: {
+			//                 sourceMap: IS_DEV
+			//             }
+			//         },
+			//         {
+			//             loader: 'sass-loader',
+			//             options: {
+			//                 sourceMap: IS_DEV,
+			//                 includePaths: [dirAssets]
+			//             }
+			//         }
+			//     ]
+			// },
+			//
+			// // IMAGES
+			// {
+			//     test: /\.(jpe?g|png|gif)$/,
+			//     loader: 'file-loader',
+			//     options: {
+			//         name: '[path][name].[ext]'
+			//     }
+			// }
+		]
+	}
 };
