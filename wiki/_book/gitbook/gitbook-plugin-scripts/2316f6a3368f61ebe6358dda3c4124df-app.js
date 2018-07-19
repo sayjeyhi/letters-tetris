8421af022a7008d15b27cdf84a765b7abaafe2f7
Mohammad Toosi <m_dd@rogers.com>
2018-07-20 02:17:36 +0430

    welcome back  _book :D

---
+++ wiki/_book/gitbook/gitbook-plugin-scripts/2316f6a3368f61ebe6358dda3c4124df-app.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(e) {});
+
+// gitbook.events.bind("page.change", function() {
+//     $(".chapter").each(function(index, elem) {
+//         var self = elem;
+//         if ($(self).find("ul.articles").length !== 0) {
+//             $(self)
+//                 .find("ul.articles")
+//                 .slideUp(0)
+//                 .addClass("hidden");
+//             $(self)
+//                 .children("*:first-child")
+//                 .append("<i class='articles-state-icon fa fa-angle-down'></i>");
+//         }
+//     });
+//     $(".chapter > *:first-child").click(function(e) {
+//         var self = this;
+//         var articles = $(self).siblings(".articles");
+//         $(self)
+//             .siblings(".articles")
+//             .stop()
+//             .toggleClass("hidden")
+//             .slideToggle();
+//         if (articles.hasClass("hidden")) {
+//             $(self)
+//                 .children(".articles-state-icon")
+//                 .removeClass("rotate");
+//         } else {
+//             $(self)
+//                 .children(".articles-state-icon")
+//                 .addClass("rotate");
+//         }
+//     });
+// });
