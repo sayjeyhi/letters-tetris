eead73257ae4459afb41b219bc2ff4d9ec3215c6
JafarAkhondali <jafar.akhondali@yahoo.com>
2018-07-20 18:28:09 +0430

    + Add weighted random for Special character choosing + Regenerate api-docs

--- src/javascript/classes/Helper.js
+++ src/javascript/classes/Helper.js
@@ -215,4 +215,25 @@ export default class Helper {
 			[array[i], array[j]] = [array[j], array[i]];
 		}
 	}
+
+	/**
+	 * This function will acept a weigthed object for randoming, and returns an function to use in generation
+	 * @param spec
+	 * @returns {function(): *}
+	 */
+	static weightedRand(spec) {
+		let i, j;
+		const table=[];
+		for (i in spec) {
+			// The constant 10 below should be computed based on the
+			// weights in the spec for a correct and optimal table size.
+			// E.g. the spec {0:0.999, 1:0.001} will break this impl.
+			for (j=0; j<spec[i]*10; j++) {
+				table.push(i);
+			}
+		}
+		return function() {
+			return table[Math.floor(Math.random() * table.length)];
+		};
+	}
 }
