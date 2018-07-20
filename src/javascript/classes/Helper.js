6a631dfee30cfe32967be3db40a0390e3d1e73f8
JafarAkhondali <jafar.akhondali@yahoo.com>
2018-07-20 11:40:39 +0430

    + Shuffle TetrisWords on build

--- src/javascript/classes/Helper.js
+++ src/javascript/classes/Helper.js
@@ -210,4 +210,16 @@ export default class Helper {
 		}
 		return -1;
 	}
+
+
+	/**
+	 * Shuffles an array
+	 * @param array
+	 */
+	static shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
 }
