65ac9a8babd192f27536c671d025c0893c4ce685
Mohammad Toosi <m_dd@rogers.com>
2018-07-21 05:50:17 +0430

    Modal tdd

--- test/classes/Modal-spec.js
+++ test/classes/Modal-spec.js
@@ -1,14 +1,12 @@
 import Modal from "../../src/javascript/classes/Modal";
 
 describe("Modal Class ", () => {
-	let modal = new Modal();
-
 	beforeAll(() => {
+		let modal = new Modal();
 		expect(Modal._createHeader).toBeDefined();
 		expect(Modal._createContent).toBeDefined();
 		expect(Modal._createFooter).toBeDefined();
 		expect(modal.show).toBeDefined();
 		expect(modal.destroy).toBeDefined();
-		expect(modal.show).toBeDefined();
 	});
 });
