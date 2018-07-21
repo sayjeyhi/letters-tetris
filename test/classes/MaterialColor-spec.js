f85bee943d5d484f84c06f6be9d649e65a71704f
Mohammad Toosi <m_dd@rogers.com>
2018-07-21 04:35:56 +0430

    material color tdd

--- test/classes/MaterialColor-spec.js
+++ test/classes/MaterialColor-spec.js
@@ -1,8 +1,13 @@
 import MaterialColor from "../../src/javascript/classes/MaterialColor";
 
-
-describe("MaterialColor class",function(){
-    it("getRandomColor method should randomly return a valid color",function(){
-        expect(/^#[0-9A-F]{6}$/i.test(MaterialColor.getRandomColor())).toBeTruthy()
-    })
-})
+describe("MaterialColor class", function() {
+	beforeAll(() => {
+		expect(MaterialColor.getRandomColor).toBeDefined();
+		expect(MaterialColor.pickRandomProperty).toBeDefined();
+	});
+	it("getRandomColor method should randomly return a valid color", function() {
+		expect(
+			/^#[0-9A-F]{6}$/i.test(MaterialColor.getRandomColor())
+		).toBeTruthy();
+	});
+});
